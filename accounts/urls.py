c04c39164b1fea1d3c1175553aa6a60087336cfd
fynxn07 <finankolamban7@gmail.com>
2026-07-25 15:52:35 +0530

    updated code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AdminLoginView,MeView,Tok
+from .views import AdminLoginView,MeView,TokenRefreshView
 
 urlpatterns = [
     path("login/", AdminLoginView.as_view(), name="admin-login"),
     path("me/", MeView.as_view(), name="me"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import AdminLoginSerializer, UserSerializer
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 
 class AdminLoginView(APIView):
